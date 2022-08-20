d4a3e10bc4ace9f3056c69916e45a630ac524c08
Lais Frigerio <laisfrigerio@hotmail.com>
2022-08-19 23:46:33 -0300

    Dia 04: Calculadora

---
+++ dia-04/script.js
@@ -0,0 +1,7 @@
+function display(value) {
+  Calculator.visor.value = Calculator.visor.value + value
+}
+
+function evaluateExpression() {
+  Calculator.visor.value = eval(Calculator.visor.value)
+}
