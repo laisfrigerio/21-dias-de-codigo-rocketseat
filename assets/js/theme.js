4373d112193da614b663cf52813b1ab168b4bc05
Lais Frigerio <laisfrigerio@hotmail.com>
2022-08-28 11:04:39 -0300

    Dia 13: adicionando arquivo theme.js no dia 08

---
+++ assets/js/theme.js
@@ -0,0 +1,33 @@
+const themeDark = 'theme-dark'
+const themeLight = 'theme-light'
+
+function getTheme() {
+  const theme = localStorage.getItem('theme')
+
+  if (theme === 'null') {
+    return themeDark
+  }
+
+  return theme
+}
+
+function setSlider(themeMode) {
+  const checked = themeMode === themeDark ? false : true
+  document.getElementById('slider').checked = checked
+}
+
+function setTheme(themeMode) {
+  localStorage.setItem('theme', themeMode)
+  document.documentElement.className = themeMode
+}
+
+function toggleTheme() {
+  const theme = getTheme()
+
+  if (theme === themeDark) {
+    setTheme(themeLight)
+    return
+  }
+
+  setTheme(themeDark)
+}
